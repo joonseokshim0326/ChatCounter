51132d7ae308bb3af835c4a70382c6c06a6242ef
jsshim0326 <37678785+joonseokshim0326@users.noreply.github.com>
2018-05-30 16:47:25 +0900

    Update ArgsErrorException.java

--- src/main/java/edu/handong/csee/java/chatcounter/ArgsErrorException.java
+++ src/main/java/edu/handong/csee/java/chatcounter/ArgsErrorException.java
@@ -1,5 +1,8 @@
 package edu.handong.csee.java.chatcounter;
-
+/**
+ * This class if for exception handling
+ * @author jssjp
+ */
 public class ArgsErrorException extends Exception {
 	public ArgsErrorException(String message) {
 		super(message);
